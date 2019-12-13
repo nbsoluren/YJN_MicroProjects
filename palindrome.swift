be8587f190b87cfda2bf8679b051b98dfc00a5d6
nbsoluren <nbsoluren@up.edu.ph>
2019-12-13 18:06:29 +0800

    Add testcases for palindrome checker

--- palindrome.swift
+++ palindrome.swift
@@ -13,3 +13,23 @@ func palindromeChecker(_ string: String) -> Bool{
 func palindromeChecker2(_ string: String) ->Bool{
   return string == String(Array(string).reversed())
 }
+
+
+
+//just tesing
+let palindromes: [String] = ["hannah", "racecar", "wow", "neveroddoreven","tacocat"]
+let notpalindromes: [String] = ["Thank you", "for", "checking", "my", "code"]
+for (positive,negative) in zip(palindromes,notpalindromes){
+  if(palindromeChecker2(positive)){ //add 2
+    print("\(positive): pass")
+  }else{
+    print("\(positive): fail")
+  }
+
+  if(!palindromeChecker2(negative)){ // add 2
+    print("\(negative): pass")
+  }else{
+    print("\(negative): fail")
+  }
+
+}
