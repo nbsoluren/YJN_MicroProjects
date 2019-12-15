2d1ff37dc5a10e57b8c3b51c24d365fea55fd93b
Natalie Nicole Soluren <nbsoluren@up.edu.ph>
2019-12-15 12:45:14 +0800

    Added MergeSort completed func

--- MergeSort/MergeSort/main.swift
+++ MergeSort/MergeSort/main.swift
@@ -15,7 +15,7 @@ func mergeTwoArrays(_ arr1: [Int],_ arr2: [Int]) -> [Int]{
     var pt2: Int = 0
     let arr1Len: Int = arr1.count
     let arr2Len: Int = arr2.count
-    while(pt1<arr1Len && pt2<arr2Len){
+    while(pt1<arr1Len || pt2<arr2Len){
         if(pt1<arr1Len && pt2>=arr2Len){
             sorted.append(arr1[pt1])
             pt1 += 1
@@ -25,20 +25,34 @@ func mergeTwoArrays(_ arr1: [Int],_ arr2: [Int]) -> [Int]{
         }else if(arr1[pt1]>arr2[pt2]){
             sorted.append(arr2[pt2])
             pt2 += 1
+        }else{
+            sorted.append(arr1[pt1])
+            pt1 += 1
         }
     }
     return sorted
 }
 //copy sorted array to main array
-func Merge(array: inout [Int], l: Int, m: Int, r: Int) -> Void {
-    let sorted = mergeTwoArrays(Array(array[l..<m]), Array(array[m...r]))
+func Merge(_ array: inout [Int], _ l: Int, _ m: Int, _ r: Int) -> Void {
+    let sorted = mergeTwoArrays(Array(array[l..<m]), Array(array[m..<r]))
     for i in l..<r{
-        array[i] = sorted[l-i]
+        array[i] = sorted[i-l]
     }
 }
 //recursive mergesort
 
-func MergeSort(array: [Int]){
-    
-    
+func MergeSort(_ array: inout [Int], _ l: Int, _ r: Int) {
+    if (r-l) < 2{
+        return
+    }
+    let m : Int = (l+r)/2
+    MergeSort(&array, l, m);
+    MergeSort(&array, m, r);
+    Merge(&array, l, m, r);
 }
+
+
+var x: [Int] = Array([1,2,3,4,5].reversed())
+print(x)
+MergeSort(&x, 0, x.count)
+print(x)
